db5487b6cfe00514e98ac07314d22ccdfcf46c23
fernandojbcba <benavidez.fernando@hotmail.com>
2023-06-15 21:58:17 -0300

    segundo

--- back/server.js
+++ back/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const estadosEstaciones = require('./index.js');
+let estadosEstaciones = require('./index.js');
 
 const app = express();
 app.use(cors());
@@ -9,7 +9,13 @@ app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.json(estadosEstaciones);
 });
-
+app.use(express.json())
+app.post('/', (req, res) => {
+  console.log(req.body)
+  estadosEstaciones = req.body; // Actualiza la variable estadosEstaciones con los datos enviados en la solicitud POST
+  res.send('Estaciones actualizadas correctamente');
+  console.log(estadosEstaciones)
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
